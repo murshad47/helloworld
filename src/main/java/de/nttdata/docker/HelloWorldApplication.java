f02ee73b57e8809d7e30c6190c8b9c51b212242e
murshad <MdMurshadul.Haque@nttdata.com>
2017-10-12 12:11:07 +0200

    project created

---
+++ src/main/java/de/nttdata/docker/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package de.nttdata.docker;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+}
